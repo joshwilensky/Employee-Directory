17ef73c1a7b252bab5cf47e167a7c02eaeab3417
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-27 16:19:06 -0400

    fixed issues

--- src/components/HelloBootstrap.js
+++ src/components/HelloBootstrap.js
@@ -4,28 +4,27 @@ function HelloBootstrap() {
   return (
     <div className="container">
       <div className="jumbotron">
-        <h1>Hello, world!</h1>
-        <p>I'm a jumbotron!</p>
+        <h1> Employee Directory </h1> <p> I'm a Little Teapot!</p>{" "}
         <p>
-          <button className="btn btn-primary btn-lg">Learn more</button>
-        </p>
-      </div>
+          <button className="btn btn-primary btn-lg"> Learn more </button>{" "}
+        </p>{" "}
+      </div>{" "}
       <div className="card mb-4">
         <div className="card-header">
-          <h3>Card title</h3>
-        </div>
+          <h3> Card Title </h3>{" "}
+        </div>{" "}
         <div className="card-body">
-          <p className="card-text">Card content</p>
-        </div>
-      </div>
+          <p className="card-text"> Card Content </p>{" "}
+        </div>{" "}
+      </div>{" "}
       <div className="card mb-4">
         <div className="card-header">
-          <h3>Card title</h3>
-        </div>
+          <h3> Card Title </h3>{" "}
+        </div>{" "}
         <div className="card-body">
-          <p className="card-text">Card content</p>
-        </div>
-      </div>
+          <p className="card-text"> Card Content </p>{" "}
+        </div>{" "}
+      </div>{" "}
     </div>
   );
 }
