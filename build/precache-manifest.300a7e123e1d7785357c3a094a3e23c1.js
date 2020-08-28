ec80d0c8c48d35520e0ec07f19a31d7d233f071e
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-27 23:04:13 -0400

    Added build folder

---
+++ build/precache-manifest.300a7e123e1d7785357c3a094a3e23c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773c5dcb91b58332ffff511446d636e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d54338a8f1c41040699",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "e36c137a4aa5aac03e31",
+    "url": "/static/js/2.96999f98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96999f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d54338a8f1c41040699",
+    "url": "/static/js/main.dddb1b40.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
