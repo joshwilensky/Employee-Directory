8c42b73835b87314d8a06e7e50d9e1084bf9e27c
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-27 22:19:18 -0400

    Updated components folder to reflect pictures and names

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+// Imports
 import React from "react";
-import HelloBootstrap from "./components/HelloBootstrap";
+import Header from "./components/Header.js";
+import Search from "./components/Search.js";
+import Table from "./components/Table.js";
+import axios from "axios";
 
 function App() {
-  return <HelloBootstrap />;
+  const [state, setState] = React.useState({
+    base: [],
+    employees: [],
+  });
+  React.useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/?results=50")
+      .then((response) => {
+        // console.log(response.data.results);
+        setState({
+          employees: response.data.results,
+          base: response.data.results,
+        });
+      })
+      .catch((error) => console.warn(error.message));
+  }, []);
+  const handleChange = (e) => {
+    const searchTerm = e.target.value;
+    setState({
+      // automatically renders
+      ...state,
+      employees: state.base.filter((employee) =>
+        employee.name.first.includes(searchTerm)
+      ),
+    });
+  };
+  return (
+    <div>
+      <Header />
+      <Search onChange={handleChange} />
+      <Table employees={state.employees} />
+    </div>
+  );
 }
-
 export default App;
---
+++ src/components/table.js
@@ -0,0 +1,35 @@
+import React from "react";
+function Table({ employees }) {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th><u>Profile Photo</u></th>
+                    <th><u>Name</u></th>
+                    <th><u>E-mail</u></th>
+                    <th><u>D.O.B.</u></th>
+                    <th><u>Location</u></th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* goes through the employee database starting at index */}
+                {employees.map((employee, i) => (
+                    <tr key={i + "-employee"}>
+                        <td>
+                            <img src={employee.picture.large} alt={employee.name.first}></img>
+                        </td>
+                        <td>
+                            {employee.name.first} {employee.name.last}
+                        </td>
+                        <td>{employee.email}</td>
+                        <td>{new Date(employee.dob.date).toLocaleDateString()}</td>
+                        <td>
+                            {employee.location.city}, {employee.location.state}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
+export default Table;
